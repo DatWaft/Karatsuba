05b5b6d66333b8298fa12c06608f077381ef6077
DatWaft <37723586+DatWaft@users.noreply.github.com>
2019-04-18 21:59:57 -0600

    Compatibilidad con diferentes bases
    Es recomendable revisar el constructor, ya que es probable que esté bugueado.

--- src/num.py
+++ src/num.py
@@ -2,6 +2,7 @@ class Num:
 	# constants
 	DEFAULT_BASE = 10
 	DEFAULT_LENGHT = 16
+	CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyz'.upper()
 
 	# constructor
 	def __init__(self, value, base = DEFAULT_BASE, lenght = DEFAULT_LENGHT):
@@ -16,11 +17,14 @@ class Num:
 			current_lenght = len(str(value))
 			if self.max_lenght < current_lenght:
 				raise Exception('The value is too big')
-			self.value = [0 for i in range(self.max_lenght - current_lenght)] + [int(x) for x in str(value)]
+			if isinstance(value, int):
+				self.value = [0 for i in range(self.max_lenght - current_lenght)] + [int(x) for x in str(value)]
+			else:
+				self.value = [0 for i in range(self.max_lenght - current_lenght)] + [int(x, base) for x in str(value)]
 	
 	# information
 	def getValue(self):
-		return Num.list_to_string(self.value)
+		return int(Num.list_to_string(self.value), self.base)
 
 	def getSize(self):
 		return len(Num.list_to_string(self.value))
@@ -33,13 +37,13 @@ class Num:
 		for i in a:
 			if not flag and i != 0:
 				flag = True
-				s += str(i)
+				s += Num.CHARACTERS[i]
 			elif flag:
-				s += str(i)
-		if s == '':
-			s = '0'
+				s += Num.CHARACTERS[i]
+		if not s:
+			s = Num.CHARACTERS[0]
 		return s
-
+	
 	# Operations
 	def add(self, num):
 		if not isinstance(num, Num):
@@ -72,7 +76,7 @@ class Num:
 		if self.base != num.base:
 			raise Exception("The base of the two of them is different.")
 		aux = self + ~num
-		if(int(self.getValue()) < int(num.getValue())):
+		if(self.getValue() < num.getValue()):
 			aux = ~aux
 		aux = aux.value[aux.getSize() - self.getSize():]
 		return Num(aux, self.base, self.max_lenght)
@@ -91,15 +95,9 @@ class Num:
 		return f"Num({Num.list_to_string(self.value)})[{self.base}]"
 
 if __name__ == "__main__":
-	n1 = Num(99)
-	n2 = Num(99)
-	print(f"{n1} + {n2} = {n1 + n2} : default = {int(n1.getValue()) + int(n2.getValue())}")
-	print(f"{n1} - {n2} = {n1 - n2} : default = {int(n1.getValue()) - int(n2.getValue())}")
-
-	for i in range(100):
-		n3 = Num(i)
-		n4 = Num(17)
-		res = int(n3.getValue()) - int(n4.getValue())
-		resn = n3 - n4
-		print(f"{n3} - {n4} = {resn} : default = {res} : {'True' if abs(res) == int(resn.getValue()) else 'ERROR'}")
-
+	n1 = Num('10', 8)
+	n2 = Num('17', 8)
+	res1 = n1 + n2
+	res2 = n1 - n2
+	print(f"{n1} + {n2} = {res1} : default = {n1.getValue() + n2.getValue()}")
+	print(f"{n1} - {n2} = {res2} : default = {n1.getValue() - n2.getValue()}")
