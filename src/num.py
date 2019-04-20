8d1b071b06906e5cdfd55cb830de980176e6c83e
DatWaft <37723586+DatWaft@users.noreply.github.com>
2019-04-20 14:30:04 -0600

    Comentarios y constructor mejorado.

--- src/num.py
+++ src/num.py
@@ -1,35 +1,57 @@
 class Num:
-	# constants
+	"""
+	La clase 'Num' simula un número de cualquier base entre 2 y 36.
+	Esta clase contiene 3 atributos:
+		- value: es una 'list' de 'int's en base 10.
+		- max_length: es un 'int' que especifíca el número máximo de elemento que puede tener 'value'.
+		- base: es un 'int' que contiene en qué base está 'value'.
+	"""
+	# Constantes.
 	DEFAULT_BASE = 10
-	DEFAULT_LENGHT = 16
-	CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyz'.upper()
+	DEFAULT_LENGTH = 16
+	CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyz'
 
-	# constructor
-	def __init__(self, value, base = DEFAULT_BASE, lenght = DEFAULT_LENGHT):
+
+	# Método constructor.
+	def __init__(self, value, base = DEFAULT_BASE, max_length = DEFAULT_LENGTH):
+		"""
+		El constructor de la clase 'Num'.
+		El parametro 'value' acepta 'int', 'str' y 'list'.
+		Si el parametro 'value' es 'int' el parametro será convertido a la base especificada.
+		El parametro 'base' especifica la base del Num, y acepta valores entre 2 y 36.
+		El parametro 'length' especifica el largo máximo que puede tener el valor dentro del Num, no puede ser menor a 1.
+		"""
+		if base > 36 or base < 2: raise Exception("Base invalida. (2 - 36)")
+		if max_length < 1: raise Exception("Largo máximo inválido, no puede ser menor a 1.")
+
 		self.base = base
-		self.max_lenght = lenght
-		if isinstance(value, list):
-			current_lenght = len(value)
-			if self.max_lenght < current_lenght:
-				raise Exception('The value is too big')
-			self.value = [0 for i in range(self.max_lenght - current_lenght)] + [int(x) for x in value]
+		self.max_length = max_length
+
+		if isinstance(value, int):
+			self.value = Num.int_to_list(value, base)
+		elif isinstance(value, str):
+			self.value = [int(x, base) for x in value]
+		elif isinstance(value, list):
+			self.value = value
 		else:
-			current_lenght = len(str(value))
-			if self.max_lenght < current_lenght:
-				raise Exception('The value is too big')
-			if isinstance(value, int):
-				self.value = [0 for i in range(self.max_lenght - current_lenght)] + [int(x) for x in str(value)]
-			else:
-				self.value = [0 for i in range(self.max_lenght - current_lenght)] + [int(x, base) for x in str(value)]
-	
-	# information
+			raise Exception("'value' es un objeto inválido.")
+		
+		if len(self.value) > max_length: raise Exception("'value' es demasiado grande.")
+		
+		self.value = [0 for i in range(self.max_length - len(self.value))] + self.value
+
+
+	# Métodos que devuelven información.
 	def getValue(self):
+		# convierte 'value' a un int en base 10.
 		return int(Num.list_to_string(self.value), self.base)
 
 	def getSize(self):
+		# devuelve el número de valores que tiene 'value' sin contar los 0s a la izquierda.
 		return len(Num.list_to_string(self.value))
 
-	# static methods
+
+	# Métodos 'static'.
 	@staticmethod
 	def list_to_string(a):
 		s = ''
@@ -43,13 +65,23 @@ class Num:
 		if not s:
 			s = Num.CHARACTERS[0]
 		return s
-	
-	# Operations
+
+	@staticmethod
+	def int_to_list(i, base):
+		i = abs(i)
+		l = []
+		while i != 0:
+			l = [i % base] + l
+			i = i//base
+		return l
+
+
+	# Métodos de operaciones.
 	def add(self, num):
 		if not isinstance(num, Num):
 			raise Exception("'num' is not a Num.")
-		if self.max_lenght != num.max_lenght:
-			raise Exception("The max lenght of the two of them is different.")
+		if self.max_length != num.max_length:
+			raise Exception("The max length of the two of them is different.")
 		if self.base != num.base:
 			raise Exception("The base of the two of them is different.")
 		base = self.base
@@ -66,24 +98,48 @@ class Num:
 		for i in self.value:
 			new += [abs(self.base - 1 - i)]
 		new[len(new) - 1] += 1
-		return Num(new, self.base, self.max_lenght)
+		return Num(new, self.base, self.max_length)
 
 	def sub(self, num):
 		if not isinstance(num, Num):
 			raise Exception("'num' is not a Num.")
-		if self.max_lenght != num.max_lenght:
-			raise Exception("The max lenght of the two of them is different.")
+		if self.max_length != num.max_length:
+			raise Exception("The max length of the two of them is different.")
 		if self.base != num.base:
 			raise Exception("The base of the two of them is different.")
 		aux = self + ~num
 		if(self.getValue() < num.getValue()):
 			aux = ~aux
 		aux = aux.value[aux.getSize() - self.getSize():]
-		return Num(aux, self.base, self.max_lenght)
+		return Num(aux, self.base, self.max_length)
+	
+	# def mul(self, num):
+	# 	if not isinstance(num, Num):
+	# 		raise Exception("'num' is not a Num.")
+	# 	if self.max_length != num.max_length:
+	# 		raise Exception("The max length of the two of them is different.")
+	# 	if self.base != num.base:
+	# 		raise Exception("The base of the two of them is different.")
+		
+	# 	res = Num(0)
+	# 	cont1 = 0
+	# 	for i in self.value[::-1]:
+	# 		cont2 = 0
+	# 		aux = Num(0)
+	# 		for j in num.value[::-1]:
+	# 			aux += Num(i * j * self.base**cont2)
+	# 			cont2 += 1
+	# 		res += Num(aux.getValue() * self.base**cont1)
+	# 		cont1 += 1
 
-	# Operators
+
+	# Operadores sobrecargados.
 	def __add__(self, num):
 		return self.add(num)
+
+	def __iadd__(self, num):
+		self = self + num
+		return self
 
 	def __invert__(self):
 		return self.invert()
@@ -91,13 +147,22 @@ class Num:
 	def __sub__(self, num):
 		return self.sub(num)
 
+	def __isub__(self, num):
+		self = self - num
+		return self
+
 	def __str__(self):
 		return f"Num({Num.list_to_string(self.value)})[{self.base}]"
 
 if __name__ == "__main__":
-	n1 = Num('10', 8)
-	n2 = Num('17', 8)
-	res1 = n1 + n2
-	res2 = n1 - n2
-	print(f"{n1} + {n2} = {res1} : default = {n1.getValue() + n2.getValue()}")
-	print(f"{n1} - {n2} = {res2} : default = {n1.getValue() - n2.getValue()}")
+	n1 = Num(15, 16)
+	print(n1.value)
+	n2 = Num('1d', 16)
+	print(n2.value)
+	print(f"n1 = {n1}")
+	print(f"n2 = {n2}")
+	print(f"n1 + n2 = {n1 + n2}")
+	n1 += n2
+	print(f"n1 = {n1}")
+	print(f"n2 = {n2}")
+
