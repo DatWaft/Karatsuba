bdc2dbacc4bbc20c1d77a323c7d7f9b891133339
datwaft <datwaftx@gmail.com>
2019-06-08 18:59:15 -0600

    Update read_test_cases.py

--- src/read_test_cases.py
+++ src/read_test_cases.py
@@ -2,7 +2,16 @@ import time
 import sys
 from num import Num
 
-def test(file_path):
+def test(file_path, mode = None):
+    def mult(expected, xval, yval):
+        return Num(expected), Num(xval) * Num(yval)
+    def div(expected, xval, yval):
+        return Num(expected), Num(xval) / Num(yval)
+    def sum(expected, xval, yval):
+        return Num(expected), Num(xval) + Num(yval)
+    def rest(expected, xval, yval):
+        return Num(expected), Num(xval) - Num(yval)
+
     print("*** Reading Test Cases ***")
     cases = None
     total = 0
@@ -19,8 +28,16 @@ def test(file_path):
             continue
         (case_num, xval, yval, expected) = (int(n) for n in case.split(";"))
         print(f"Processing case {case_num}")
-        expected = Num(expected)
-        given = Num(xval) * Num(yval)
+
+        if not mode or mode == 'mult':
+            expected, given = mult(expected, xval, yval)
+        elif mode == 'div':
+            expected, given = div(expected, xval, yval)
+        elif mode == 'sum':
+            expected, given = sum(expected, xval, yval)
+        elif mode == 'rest':
+            expected, given = rest(expected, xval, yval)
+
         if  given != expected:
             print(f"*** Case {case_num} failed! {given} != {expected} ***")
             failed += 1
@@ -35,8 +52,13 @@ def test(file_path):
 
 if __name__ == "__main__":
     argv = sys.argv
+    mode = None
     if len(argv) == 1:
         file = "../test/test_01.csv"
+    elif len(argv) == 2:
+        file = argv[1]
     else:
         file = argv[1]
-    test(file)
+        mode = argv[2]
+
+    test(file, mode)
