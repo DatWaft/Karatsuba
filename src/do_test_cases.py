c020db929e8678f097d82b087292d07f96988ad9
fernandagonzalezarias <ferga0017@gmail.com>
2019-06-10 11:09:42 -0600

    Nombres

--- src/do_test_cases.py
+++ src/do_test_cases.py
@@ -1,4 +1,11 @@
 from read_test_cases import test
+"""
+Autores:
+  - Rebecca Garita Gutiérrez        117620191   Martes y Viernes, 10 AM
+  - David Alberto Guevara Sánchez   402450355   Martes y Viernes, 10 AM
+  - M. Fernanda Gonzáles Arias      117660980   Martes y Viernes, 10 AM
+  - Luis David Villalobos Gonzáles  117540697   Martes y Viernes, 10 AM
+"""
 
 def do_tests():
 	print("*** Doing test cases for multiplication ***")
